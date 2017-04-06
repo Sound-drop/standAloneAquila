e00eb1c81d5f758bee75e8b7510de324872b6b1d
Mick Lin <cl3469@columbia.edu>
2017-04-06 13:53:46 -0400

    track 3 freq

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@
 #include <cstdlib>
 using namespace std;
 
-void plotSpectrum(Aquila::SpectrumType spectrum, Aquila::FrequencyType sampleFreq)
+const std::size_t SIZE = 512;
+
+void findMax(Aquila::SpectrumType spectrum, Aquila::FrequencyType sampleFreq)
     {
         std::size_t halfLength = spectrum.size() / 2;
         std::vector<double> absSpectrum(halfLength);
@@ -31,6 +33,33 @@ void plotSpectrum(Aquila::SpectrumType spectrum, Aquila::FrequencyType sampleFre
         //plot(absSpectrum);
     }
 
+void freqOfindex(std::size_t start, Aquila::WaveFile wav){
+    Aquila::FrequencyType sampleFreq = wav.getSampleFrequency();
+    vector<Aquila::SampleType> chunk;
+    for (std::size_t i =start; i< start+SIZE; ++i)
+    {   
+
+        chunk.push_back(wav.sample(i));
+
+    }
+    Aquila::SignalSource data(chunk, sampleFreq);
+
+   
+
+    Aquila::TextPlot plt("input wave");
+
+ 
+    
+    auto fft = Aquila::FftFactory::getFft(SIZE);
+    Aquila::SpectrumType spectrum = fft->fft(data.toArray());
+    plt.setTitle("Signal spectrum of "+ std::to_string(start));
+
+
+    findMax(spectrum, sampleFreq);
+    plt.plotSpectrum(spectrum);
+
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
@@ -41,75 +70,16 @@ int main(int argc, char *argv[])
 
     Aquila::WaveFile wav(argv[1]);
 
-
-    const Aquila::FrequencyType sampleFreq = wav.getSampleFrequency();
-    const std::size_t SIZE = 64;
     const std::size_t END = wav.getSamplesCount();
+    std::size_t start = 0;
+
+    while(start < END && wav.sample(start) <= 1000 ) start++;
     //220500
-    //cout << END;
-    const Aquila::FrequencyType f_lp = 500;
+    cout << END <<endl;
+    cout << start;
 
-    vector<Aquila::SampleType> chunk;
-    for (std::size_t i =END-SIZE; i< END; ++i)
-    {	
-    	chunk.push_back(wav.sample(i));
-
-    }
-    Aquila::SignalSource data(chunk, sampleFreq);
-    // std::cout << "Maximum sample value: " << maxValue << std::endl;
-
-	// const Aquila::FrequencyType sampleFreq = 2000;
-    //  const Aquila::FrequencyType f1 = 180, f2 = 300;
+    for(int x = start; x < END; x+=END/5) freqOfindex(x, wav);
 
    
-
-    Aquila::TextPlot plt("input wave");
-
- 
-    
-    auto fft = Aquila::FftFactory::getFft(SIZE);
-    Aquila::SpectrumType spectrum = fft->fft(data.toArray());
-    plt.setTitle("Signal spectrum before filtration");
-
-
-    plotSpectrum(spectrum, sampleFreq);
-    plt.plotSpectrum(spectrum);
-
-    // generate a low-pass filter spectrum
-    Aquila::SpectrumType filterSpectrum(SIZE);
-    for (std::size_t i = 0; i < SIZE; ++i)
-    {
-        if (i < (SIZE * f_lp / sampleFreq))
-        {
-            // passband
-            filterSpectrum[i] = 1.0;
-        }
-        else
-        {
-            // stopband
-            filterSpectrum[i] = 0.0;
-        }
-    }
-    plt.setTitle("Filter spectrum");
-    // plt.plotSpectrum(filterSpectrum);
-
-    // the following call does the multiplication of two spectra
-    // (which is complementary to convolution in time domain)
-    std::transform(
-        std::begin(spectrum),
-        std::end(spectrum),
-        std::begin(filterSpectrum),
-        std::begin(spectrum),
-        [] (Aquila::ComplexType x, Aquila::ComplexType y) { return x * y; }
-    );
-    plt.setTitle("Signal spectrum after filtration");
-    // plt.plotSpectrum(spectrum);
-
-    // Inverse FFT moves us back to time domain
-    double x1[SIZE];
-    fft->ifft(spectrum, x1);
-    plt.setTitle("Signal waveform after filtration");
-    //plt.plot(x1, SIZE);
-
     return 0;
 }
